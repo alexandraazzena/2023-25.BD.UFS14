59410f25801d2d0baeced5b3cbc5a9352563b183
Alexandra Azzena <168538339+alexandraazzena@users.noreply.github.com>
2024-09-27 14:56:20 +0000

    test sul project work

--- projectwork_test/test_file_function.py
+++ projectwork_test/test_file_function.py
@@ -1,14 +1,16 @@
 from file_function import controllo_validita_url, crea_dizionario
+from jsonschema import validate
 
 # ASSERT
 def test_controllo_validita_url():
     assert controllo_validita_url('https://google.com')== True
     assert controllo_validita_url('http://google.com')== True
     assert controllo_validita_url('https//google.com')== False
+    #assert controllo_validita_url('https://goo!!gle.com')== False
 
 
-# JSON SCHEMA
-def test_crea_diz():
+# JSON SCHEMA SENZA VALIDATE
+'''def test_crea_diz():
 
     # Esecuzione della funzione da testare
     output = crea_dizionario([('a','b'), ('c', 'd'), ('e', 'f')])
@@ -23,6 +25,29 @@ def test_crea_diz():
     # Verifica che ogni valore sia una stringa
     for valore in output.values():
         assert isinstance(valore, str) == True
+'''
+
+# JSON SCHEMA CON VALIDATE
+schema = {
+    "type" : "object",
+    "properties" : {
+        "nomi" : {"type" : "string"},
+        "link" : {"type" : "string"},
+    },
+}
+
+# TEST
+def test_jsonschema_success():
+    assert my_validate(instance={"nomi" : "aaa", "link" : "bbb"}, schema=schema) == True
+
+# WRAPPER 
+def my_validate(instance, schema):
+    try:
+        validate(instance=instance, schema=schema)
+        return True
+    except: 
+        return False
+
 
 # SNAPSHOT
 def test_function_output_with_snapshot(snapshot):
