6351255800b9a1ad7f2f043452ba688a9fbb6704
Alexandra Azzena <168538339+alexandraazzena@users.noreply.github.com>
2024-09-27 13:59:19 +0000

    cccc

--- projectwork_test/test_file_function.py
+++ projectwork_test/test_file_function.py
@@ -1,22 +1,30 @@
 from file_function import controllo_validita_url, crea_dizionario
 
+# ASSERT
 def test_controllo_validita_url():
     assert controllo_validita_url('https://google.com')== True
     assert controllo_validita_url('http://google.com')== True
     assert controllo_validita_url('https//google.com')== False
 
+
+# JSON SCHEMA
 def test_crea_diz():
 
     # Esecuzione della funzione da testare
-    output = crea_dizionario(nomi_links)
+    output = crea_dizionario([('a','b'), ('c', 'd'), ('e', 'f')])
 
     # Verifica che il risultato sia un dizionario
-    assert isinstance(output, dict), "Il risultato non è un dizionario"
+    assert isinstance(output, dict) == True
 
     # Verifica che ogni chiave sia una stringa
     for chiave in output.keys():
-        assert isinstance(chiave, str), f"La chiave {chiave} non è una stringa"
+        assert isinstance(chiave, str) == True
 
     # Verifica che ogni valore sia una stringa
     for valore in output.values():
-        assert isinstance(valore, str), f"Il valore {valore} non è una stringa"
+        assert isinstance(valore, str) == True
+
+# SNAPSHOT
+def test_function_output_with_snapshot(snapshot):
+    output = str(crea_dizionario([('a','b'), ('c', 'd'), ('e', 'f')]))
+    snapshot.assert_match(output, 'project_work_snap.txt')
