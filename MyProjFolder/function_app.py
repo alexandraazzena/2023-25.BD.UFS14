a4fc4122067bbcb7bf0302bbaf616174bd4e6a52
alexitsrizzoli <alexandra.azzena@itsrizzoli.it>
2024-10-28 15:48:30 +0000

    func pw

--- MyProjFolder/function_app.py
+++ MyProjFolder/function_app.py
@@ -1,4 +1,3 @@
-
 import azure.functions as func
 import datetime
 import json
@@ -6,13 +5,13 @@ import logging
 
 app = func.FunctionApp()
 
-@app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.ANONYMOUS)
-def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
+
+@app.route(route="HttpExample", auth_level=func.AuthLevel.ANONYMOUS)
+def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
-    cognome = req.params.get('cognome')
-
+    url_da_validare = req.params.get('url_da_validare')
     if not name:
         try:
             req_body = req.get_json()
@@ -21,13 +20,21 @@ def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
-    if name:
-        if cognome:
-            return func.HttpResponse(f"Hello, {name} {cognome}. This HTTP triggered function executed successfully.")
-        else:
-            return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    '''if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "Alex Alex Alexaaaaa.",
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
-        )
+        )'''
+    if url_da_validare:
+        url_valido = controllo_validita_url(url_da_validare)
+        if url_valido:
+            return func.HttpResponse(f"URL valido")
+        else:
+             return func.HttpResponse('URL non valido')
+    
+    
+    
+def controllo_validita_url(url):
+    return url.startswith('http://') or url.startswith('https://')
